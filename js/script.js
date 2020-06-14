25063a228da93697d3a640831b9dcf494e15e3b7
Sainthedgehog1 <60622449+Sainthedgehog1@users.noreply.github.com>
2020-06-14 10:53:55 +0300

    1 and 2 page compleat, reload, fix bug

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 // Кнопка выпадающего меню //
 
 var showNavigationLink = document.querySelector('.catalog-button');
+var showNavigationLinkHover = document.querySelector('.catalog-button-hover');
 var showNavigation = document.querySelector('.site-nav__sublist');
 
+showNavigationLinkHover.classList.remove('catalog-button-hover');
 showNavigationLink.addEventListener('click', function (evt) {
   evt.preventDefault();
   showNavigation.classList.toggle('sublist-visible');
@@ -37,15 +39,29 @@ var modalForm = document.querySelector('.modal-feedback');
 var modalMap = document.querySelector('.modal-map');
 var formClose = modalForm.querySelector('.modal-close');
 var mapClose = modalMap.querySelector('.modal-close');
+var feedbackForm = modalForm.querySelector(".modal-feedback__form");
+var modalFormName = modalForm.querySelector(".modal-feedback__form__input-name");
+var modalFormEmail = modalForm.querySelector(".modal-feedback__form__input-email");
+var modalFormComment = modalForm.querySelector(".modal-feedback__form__input-comment");
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
 linkForm.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalForm.classList.add('modal-show');
+  modalFormName.focus();
 });
 
 formClose.addEventListener('click', function (evt) {
   evt.preventDefault();
   modalForm.classList.remove('modal-show');
+  modalForm.classList.remove("modal-error");
 });
 
 linkMap.addEventListener('click', function (evt) {
@@ -58,11 +74,27 @@ mapClose.addEventListener('click', function (evt) {
   modalMap.classList.remove('modal-show');
 });
 
+feedbackForm.addEventListener("submit", function (evt) {
+  if (!modalFormName.value || !modalFormEmail.value || !modalFormComment.value) {
+    evt.preventDefault();
+    setTimeout(function () {
+      modalForm.classList.remove("modal-error");
+    }, 1000);
+    modalForm.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", modalFormEmail.value);
+    }
+  }
+});
+
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (modalForm.classList.contains('modal-show')) {
+      evt.preventDefault();
       modalForm.classList.remove('modal-show');
+      modalForm.classList.remove("modal-error");
     }
     if (modalMap.classList.contains('modal-show')) {
       modalMap.classList.remove('modal-show');
